4444cfbdcb26d99aec07028b3c9b4ce9e539b5c3
Seoyeon <eternalhymns@gmail.com>
2021-01-15 13:43:07 +0900

    사이드바 아이콘 생성 및 버튼 클릭 가능하게 하였음

---
+++ src/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Sidebar.css";
+
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MorevertIcon from "@material-ui/icons/MoreVert";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MorevertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDkxPJcw4MCv1Z2JTjtil-ETZxG0P3ycy8",
+  authDomain: "whatsapp-effcb.firebaseapp.com",
+  projectId: "whatsapp-effcb",
+  storageBucket: "whatsapp-effcb.appspot.com",
+  messagingSenderId: "615622785454",
+  appId: "1:615622785454:web:5cbad86aa1fee196aff03f",
+  measurementId: "G-ER0RDHGQ6B"
+};
